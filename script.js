3e5055ccf4f16eee77a67beef9c41496594e0456
Matthew <goehnermatthew@gmail.com>
2022-11-11 16:12:28 -0500

    Started using turoial

---
+++ script.js
@@ -0,0 +1,24 @@
+var DIRECTION = {
+	IDLE: 0,
+	UP: 1,
+	DOWN: 2,
+	LEFT: 3,
+	RIGHT: 4
+};
+
+var rounds = [5, 5, 3, 3, 2];
+var colors = ['#1abc9c', '#2ecc71', '#3498db', '#e74c3c', '#9b59b6'];
+
+var Ball = {
+	new: function (incrementedSpeed) {
+		return {
+			width: 5,
+			height: 5,
+			x: (this.canvas.width / 2) - 9,
+			y: (this.canvas.height / 2) - 9,
+			moveX: DIRECTION.IDLE,
+			moveY: DIRECTION.IDLE,
+			speed: incrementedSpeed || 9
+		};
+	}
+};
